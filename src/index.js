f5642950b27127c3c4a8a51bbc64691f6cbed0d4
AlexandrFugin <fav2.7@mail.ru>
2022-08-02 22:38:55 +0300

    fix: partial execution of a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+const firstElement = ['(', '{', '[', '|', '1', '3', '5', '7', '8'];
+const pairElement = {
+  ')': '(',
+  '}': '{',
+  ']': '[',
+  '|': '|',
+  '2': '1',
+  '4': '3',
+  '6': '5', 
+  '7': '7', 
+  '8': '8', 
+};
+let res = [];
+
+for (let i = 0; i < str.length; i++) { 
+  if (firstElement.includes(str[i])) {
+    res.push(str[i]);
+  } else {
+    if (res.length === 0) {
+      return false;
+    }
+    // if (str[i] === res[res.length - 1]) {
+    //   res.pop();
+    // } else {
+    
+      if (pairElement[str[i]] === res[res.length - 1] ) {
+        res.pop();
+        if (res[res.length - 1] === '|') {
+        return true; }
+      } else {
+        return false;
+      // } 
+    }
+  }
+}
+// console.log(res);
+return res.length === 0;
 }
